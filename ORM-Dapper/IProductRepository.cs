be57a66ac4e54cf11d7aec341633551299151983
Michael Driscoll <michael.driscoll951@gmail.com>
2023-06-15 14:01:14 -0700

    hw

---
+++ ORM-Dapper/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ORM_Dapper
+{
+	public interface IProductRepository
+	{
+		public IEnumerable<Product> GetAllProducts();
+
+
+		public void CreateProduct(string name, double price, int categoryID);
+
+
+		public void DeleteProduct(int productID);
+	}
+}
+
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
+using ORM_Dapper;
 
 var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -10,3 +11,35 @@ var config = new ConfigurationBuilder()
 string connString = config.GetConnectionString("DefaultConnection");
 
 IDbConnection conn = new MySqlConnection(connString);
+
+
+var repo = new DapperProductRepository(conn);
+
+Console.WriteLine("what is the name of your new product");
+var prodName = Console.ReadLine();
+
+Console.WriteLine("what is the price");
+var prodPrice = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("what is the catagory id?");
+var prodCat = int.Parse(Console.ReadLine());
+
+repo.CreateProduct(prodName, prodPrice, prodCat);
+
+var prodList = repo.GetAllProducts();
+
+foreach(var prod in prodList)
+{
+    Console.WriteLine($"{prod.ProductID} - {prod.Name}");
+}
+Console.WriteLine("what is the Product ID you want to update");
+var prodID = int.Parse(Console.ReadLine());
+
+Console.WriteLine("what is the new product name");
+var newName = Console.ReadLine();
+
+repo.UpdateProduct(prodID, newName);
+
+Console.WriteLine("what is the product ID you want to delete?");
+prodID = int.Parse(Console.ReadLine());
